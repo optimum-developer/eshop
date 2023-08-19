3c3f78677f797fcf942b9f406d30a485e7609426
Parmeet Singh <cdeveloper2016@gmail.com>
2023-08-19 19:02:27 +0530

    wip product variant

--- backend/controller/product.js
+++ backend/controller/product.js
@@ -1,247 +1,262 @@
-const express = require("express");
+const express = require('express');
 const {
-  isSeller,
-  isAuthenticated,
-  isAdminAuthenticated,
-  isAdmin,
-} = require("../middleware/auth");
-const catchAsyncErrors = require("../middleware/catchAsyncErrors");
+	isSeller,
+	isAuthenticated,
+	isAdminAuthenticated,
+	isAdmin,
+} = require('../middleware/auth');
+const catchAsyncErrors = require('../middleware/catchAsyncErrors');
 const router = express.Router();
-const Product = require("../model/product");
-const Order = require("../model/order");
-const Shop = require("../model/shop");
-const { upload } = require("../multer");
-const ErrorHandler = require("../utils/ErrorHandler");
-const sendMail = require("../utils/sendMail");
-const fs = require("fs");
+const Product = require('../model/product');
+const Order = require('../model/order');
+const Shop = require('../model/shop');
+const { upload } = require('../multer');
+const ErrorHandler = require('../utils/ErrorHandler');
+const sendMail = require('../utils/sendMail');
+const fs = require('fs');
 
 // create product
 router.post(
-  "/create-product",
-  upload.array("images"),
-  catchAsyncErrors(async (req, res, next) => {
-    try {
-      const shopId = req.body.shopId;
-      const { asin, images } = req.body;
-      console.log(typeof asin);
-      console.log({ asin });
+	'/create-product',
+	upload.array('images'),
+	catchAsyncErrors(async (req, res, next) => {
+		try {
+			const shopId = req.body.shopId;
+			const { asin, images, variant } = req.body;
+			console.log(typeof asin);
+			console.log({ asin });
+			console.log("variant--", variant)
+			const shop = await Shop.findById(shopId);
+			// let asinNo;
+			// const asinNo = await Product.findOne({ asin: asin });
 
-      const shop = await Shop.findById(shopId);
-      // let asinNo;
-      // const asinNo = await Product.findOne({ asin: asin });
+			if (asin == 'null') {
+				const products = await Product.find().sort({ asin: -1 });
+				req.body.asin = products[0].asin + 1;
+			}
 
-      if (asin == "null") {
-        const products = await Product.find().sort({ asin: -1 });
-        req.body.asin = products[0].asin + 1;
-      }
+			if (!shop) {
+				return next(new ErrorHandler('Shop Id is invalid!', 400));
+			} else {
+				const files = req.files;
+				console.log('FILES--', files);
 
-      if (!shop) {
-        return next(new ErrorHandler("Shop Id is invalid!", 400));
-      } else {
-        const files = req.files;
-        const imageUrls = files.map((file) => `${file.filename}`);
+				const imageUrls = files.map((file) => `${file.filename}`);
 
-        const productData = req.body;
-        productData.images = asin !== "null" ? images : imageUrls;
-        productData.shop = shop;
-        console.log(productData.images);
+				const productData = req.body;
 
-        // return
-        const product = await Product.create(productData);
+				productData.images = asin !== 'null' ? images : imageUrls;
+				productData.shop = shop;
 
-        res.status(201).json({
-          success: true,
-          product,
-        });
-      }
-    } catch (error) {
-      console.log("catch error");
-      return next(new ErrorHandler(error, 400));
-    }
-  })
+				productData.variant = JSON.parse(productData.variant);
+				console.log("variant", productData.variant.images)
+				// productData.variant.map((variantItem) => {
+				// 	variantItem.images.map((image) => {
+				// 		console.log("variant image", image)
+				// 	})
+				// 	});
+
+
+				// return
+				const product = await Product.create(productData);
+
+				res.status(201).json({
+					success: true,
+					product,
+				});
+			}
+		} catch (error) {
+			console.log('catch error');
+			return next(new ErrorHandler(error, 400));
+		}
+	})
 );
 
 // get all products of a shop
 router.get(
-  "/get-all-products-shop/:id",
-  catchAsyncErrors(async (req, res, next) => {
-    try {
-      const products = await Product.find({ shopId: req.params.id });
+	'/get-all-products-shop/:id',
+	catchAsyncErrors(async (req, res, next) => {
+		try {
+			const products = await Product.find({ shopId: req.params.id });
 
-      res.status(201).json({
-        success: true,
-        products,
-      });
-    } catch (error) {
-      return next(new ErrorHandler(error, 400));
-    }
-  })
+			res.status(201).json({
+				success: true,
+				products,
+			});
+		} catch (error) {
+			return next(new ErrorHandler(error, 400));
+		}
+	})
 );
 
 // delete product of a shop
 router.delete(
-  "/delete-shop-product/:id",
-  isSeller,
-  catchAsyncErrors(async (req, res, next) => {
-    try {
-      const productId = req.params.id;
+	'/delete-shop-product/:id',
+	isSeller,
+	catchAsyncErrors(async (req, res, next) => {
+		try {
+			const productId = req.params.id;
 
-      const productData = await Product.findById(productId);
+			const productData = await Product.findById(productId);
 
-      productData.images.forEach((imageUrl) => {
-        const filename = imageUrl;
-        const filePath = `uploads/${filename}`;
+			productData.images.forEach((imageUrl) => {
+				const filename = imageUrl;
+				const filePath = `uploads/${filename}`;
 
-        fs.unlink(filePath, (err) => {
-          if (err) {
-            console.log(err);
-          }
-        });
-      });
+				fs.unlink(filePath, (err) => {
+					if (err) {
+						console.log(err);
+					}
+				});
+			});
 
-      const product = await Product.findByIdAndDelete(productId);
+			const product = await Product.findByIdAndDelete(productId);
 
-      if (!product) {
-        return next(new ErrorHandler("Product not found with this id!", 500));
-      }
+			if (!product) {
+				return next(
+					new ErrorHandler('Product not found with this id!', 500)
+				);
+			}
 
-      res.status(201).json({
-        success: true,
-        message: "Product Deleted successfully!",
-      });
-    } catch (error) {
-      return next(new ErrorHandler(error, 400));
-    }
-  })
+			res.status(201).json({
+				success: true,
+				message: 'Product Deleted successfully!',
+			});
+		} catch (error) {
+			return next(new ErrorHandler(error, 400));
+		}
+	})
 );
 
 // get all products
 router.get(
-  "/get-all-products",
-  catchAsyncErrors(async (req, res, next) => {
-    try {
-      const products = await Product.find({ approval: "approved" }).sort({
-        createdAt: -1,
-      });
+	'/get-all-products',
+	catchAsyncErrors(async (req, res, next) => {
+		try {
+			const products = await Product.find({ approval: 'approved' }).sort({
+				createdAt: -1,
+			});
 
-      res.status(201).json({
-        success: true,
-        products,
-      });
-    } catch (error) {
-      return next(new ErrorHandler(error, 400));
-    }
-  })
+			res.status(201).json({
+				success: true,
+				products,
+			});
+		} catch (error) {
+			return next(new ErrorHandler(error, 400));
+		}
+	})
 );
 
 // review for a product
 router.put(
-  "/create-new-review",
-  isAuthenticated,
-  catchAsyncErrors(async (req, res, next) => {
-    try {
-      const { user, rating, comment, productId, orderId } = req.body;
+	'/create-new-review',
+	isAuthenticated,
+	catchAsyncErrors(async (req, res, next) => {
+		try {
+			const { user, rating, comment, productId, orderId } = req.body;
 
-      const product = await Product.findById(productId);
+			const product = await Product.findById(productId);
 
-      const review = {
-        user,
-        rating,
-        comment,
-        productId,
-      };
+			const review = {
+				user,
+				rating,
+				comment,
+				productId,
+			};
 
-      const isReviewed = product.reviews.find(
-        (rev) => rev.user._id === req.user._id
-      );
+			const isReviewed = product.reviews.find(
+				(rev) => rev.user._id === req.user._id
+			);
 
-      if (isReviewed) {
-        product.reviews.forEach((rev) => {
-          if (rev.user._id === req.user._id) {
-            (rev.rating = rating), (rev.comment = comment), (rev.user = user);
-          }
-        });
-      } else {
-        product.reviews.push(review);
-      }
+			if (isReviewed) {
+				product.reviews.forEach((rev) => {
+					if (rev.user._id === req.user._id) {
+						(rev.rating = rating),
+							(rev.comment = comment),
+							(rev.user = user);
+					}
+				});
+			} else {
+				product.reviews.push(review);
+			}
 
-      let avg = 0;
+			let avg = 0;
 
-      product.reviews.forEach((rev) => {
-        avg += rev.rating;
-      });
+			product.reviews.forEach((rev) => {
+				avg += rev.rating;
+			});
 
-      product.ratings = avg / product.reviews.length;
+			product.ratings = avg / product.reviews.length;
 
-      await product.save({ validateBeforeSave: false });
+			await product.save({ validateBeforeSave: false });
 
-      await Order.findByIdAndUpdate(
-        orderId,
-        { $set: { "cart.$[elem].isReviewed": true } },
-        { arrayFilters: [{ "elem._id": productId }], new: true }
-      );
+			await Order.findByIdAndUpdate(
+				orderId,
+				{ $set: { 'cart.$[elem].isReviewed': true } },
+				{ arrayFilters: [{ 'elem._id': productId }], new: true }
+			);
 
-      res.status(200).json({
-        success: true,
-        message: "Reviwed succesfully!",
-      });
-    } catch (error) {
-      return next(new ErrorHandler(error, 400));
-    }
-  })
+			res.status(200).json({
+				success: true,
+				message: 'Reviwed succesfully!',
+			});
+		} catch (error) {
+			return next(new ErrorHandler(error, 400));
+		}
+	})
 );
 
 // all products --- for admin
 router.get(
-  "/admin-all-products",
-  isAdminAuthenticated,
-  catchAsyncErrors(async (req, res, next) => {
-    try {
-      const products = await Product.find().sort({
-        createdAt: -1,
-      });
-      res.status(201).json({
-        success: true,
-        products,
-      });
-    } catch (error) {
-      return next(new ErrorHandler(error.message, 500));
-    }
-  })
+	'/admin-all-products',
+	isAdminAuthenticated,
+	catchAsyncErrors(async (req, res, next) => {
+		try {
+			const products = await Product.find().sort({
+				createdAt: -1,
+			});
+			res.status(201).json({
+				success: true,
+				products,
+			});
+		} catch (error) {
+			return next(new ErrorHandler(error.message, 500));
+		}
+	})
 );
 
 router.put(
-  "/update-approval-status",
-  isAdminAuthenticated,
-  catchAsyncErrors(async (req, res, next) => {
-    try {
-      const { id, value, name, email } = req.body;
+	'/update-approval-status',
+	isAdminAuthenticated,
+	catchAsyncErrors(async (req, res, next) => {
+		try {
+			const { id, value, name, email } = req.body;
 
-      const product = await Product.findByIdAndUpdate(
-        { _id: id },
-        { approval: value },
-        { new: true }
-      );
+			const product = await Product.findByIdAndUpdate(
+				{ _id: id },
+				{ approval: value },
+				{ new: true }
+			);
 
-      await product.save();
+			await product.save();
 
-      const products = await Product.find().sort({
-        createdAt: -1,
-      });
+			const products = await Product.find().sort({
+				createdAt: -1,
+			});
 
-      await sendMail({
-        email: email,
-        subject: `Product Approval`,
-        message: `Dear ${name},Your product ${product.name} has been ${value}`,
-      });
+			await sendMail({
+				email: email,
+				subject: `Product Approval`,
+				message: `Dear ${name},Your product ${product.name} has been ${value}`,
+			});
 
-      res.status(201).json({
-        success: true,
-        products,
-      });
-    } catch (error) {
-      return next(new ErrorHandler(error.message, 500));
-    }
-  })
+			res.status(201).json({
+				success: true,
+				products,
+			});
+		} catch (error) {
+			return next(new ErrorHandler(error.message, 500));
+		}
+	})
 );
 module.exports = router;
--- backend/model/product.js
+++ backend/model/product.js
@@ -1,93 +1,117 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
 const productSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, "Please enter your product name!"],
-  },
-  description: {
-    type: String,
-    required: [true, "Please enter your product description!"],
-  },
-  category: {
-    type: String,
-    required: [true, "Please enter your product category!"],
-  },
-  tags: {
-    type: String,
-  },
-  color: {
-    type: String,
-  },
-  brand: {
-    type: String,
-  },
-  size: {
-    type: String,
-  },
-  originalPrice: {
-    type: Number,
-  },
-  discountPrice: {
-    type: Number,
-    required: [true, "Please enter your product price!"],
-  },
-  stock: {
-    type: Number,
-    required: [true, "Please enter your product stock!"],
-  },
-  images: [
-    {
-      type: String,
-    },
-  ],
-  reviews: [
-    {
-      user: {
-        type: Object,
-      },
-      rating: {
-        type: Number,
-      },
-      comment: {
-        type: String,
-      },
-      productId: {
-        type: String,
-      },
-      createdAt: {
-        type: Date,
-        default: Date.now(),
-      },
-    },
-  ],
-  asin: {
-    type: Number,
-    required: true,
-  },
-  ratings: {
-    type: Number,
-  },
-  shopId: {
-    type: String,
-    required: true,
-  },
-  shop: {
-    type: Object,
-    required: true,
-  },
-  sold_out: {
-    type: Number,
-    default: 0,
-  },
-  approval: {
-    type: String,
-    default: "pending",
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now(),
-  },
+	name: {
+		type: String,
+		required: [true, 'Please enter your product name!'],
+	},
+	description: {
+		type: String,
+		required: [true, 'Please enter your product description!'],
+	},
+	category: {
+		type: String,
+		required: [true, 'Please enter your product category!'],
+	},
+	tags: {
+		type: String,
+	},
+	color: {
+		type: String,
+	},
+	brand: {
+		type: String,
+	},
+	size: {
+		type: String,
+	},
+	originalPrice: {
+		type: Number,
+	},
+	discountPrice: {
+		type: Number,
+		required: [true, 'Please enter your product price!'],
+	},
+	stock: {
+		type: Number,
+		required: [true, 'Please enter your product stock!'],
+	},
+	images: [
+		{
+			type: String,
+		},
+	],
+	reviews: [
+		{
+			user: {
+				type: Object,
+			},
+			rating: {
+				type: Number,
+			},
+			comment: {
+				type: String,
+			},
+			productId: {
+				type: String,
+			},
+			createdAt: {
+				type: Date,
+				default: Date.now(),
+			},
+		},
+	],
+	asin: {
+		type: Number,
+		required: true,
+	},
+	ratings: {
+		type: Number,
+	},
+	shopId: {
+		type: String,
+		required: true,
+	},
+	shop: {
+		type: Object,
+		required: true,
+	},
+	sold_out: {
+		type: Number,
+		default: 0,
+	},
+	approval: {
+		type: String,
+		default: 'pending',
+	},
+	variant: [
+		{
+			size: {
+				type: String,
+			},
+			color: {
+				type: String,
+			},
+			price: {
+				type: Number,
+			},
+			discount: {
+				type: Number,
+			},
+			stock: {
+				type: Number,
+			},
+			images: [
+				{
+					type: String,
+				},
+			],
+		},
+	],
+	createdAt: {
+		type: Date,
+		default: Date.now(),
+	},
 });
 
-module.exports = mongoose.model("Product", productSchema);
+module.exports = mongoose.model('Product', productSchema);
